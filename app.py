9f1fe8c3817ba3d59ae3113af21a0f6b302bd3ea
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-04-30 14:31:19 +0200

    Add forms and request methods

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
+
+users = {
+    "archie.andrews@email.com": "football4life",
+    "veronica.lodge@email.com": "fashiondiva"
+}
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        if email in users and users[email] == password:
+            return render_template("login.html", message="Successfully logged in")
+    else:
+        return render_template("login.html", message="Incorrect login")
+    return render_template("login.html")
+
 
 @app.route("/about")
 def about():
