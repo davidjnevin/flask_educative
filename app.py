ae3cbc056e7780c5bfec1dda581d7b533d011c5f
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-04-30 11:53:58 +0200

    Template inheritance

--- app.py
+++ app.py
@@ -1,39 +1,13 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
-
 @app.route("/")
-def index():
-    Username = "Sally"
-    return render_template("index.html", username=Username)
-
-
-@app.route("/dict")
-def index_dict():
-    Users = {"Archie": "Amsterdam", "Veronica": "London", "Betty": "San Francisco", "Jughead": "Los Angeles"}
-    return render_template("index_dict.html", users=Users)
-
-
-@app.route("/home")
 def home():
     return render_template("home.html")
 
-
-@app.route("/flask")
-def flask():
-    return "Hello, flask!"
-
-
-@app.route("/<myname>")
-def greetings(myname):
-    return f"Hello, {myname}!"
-
-
-@app.route("/square/<int:number>")
-def square(number):
-    return f"The square of {str(number)} is {str(number**2)}!"
-
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
