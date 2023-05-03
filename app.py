fc7b9a2c6742f5e7bdf25812ffd87667e8009429
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-05-03 10:09:45 +0200

    Add db table, 1-1, 1-many, many-many

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 from forms import LoginForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dfewfew123213rwdsgert34tgfd1234trgf'
+app.config["SECRET_KEY"] = "dfewfew123213rwdsgert34tgfd1234trgf"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example_2.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+class Employee(db.Model):
+    employee_id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String(50), nullable = False)
+    last_name = db.Column(db.String(50), nullable = False)
+    department_name = db.Column(db.String(50), db.ForeignKey('department.name'), nullable = False)
+    is_head_of = db.Column(db.String, db.ForeignKey('department.name'), nullable=True)
+
+class Department(db.Model):
+    name = db.Column(db.String(50), primary_key=True, nullable = False)
+    location = db.Column(db.String(120), nullable = False)
+    employees = db.relationship('Employee', backref = 'department')
+    head = db.relationship('Employee', backref='head_of_department', uselist=False)
+
+class Project(db.Model):
+    project_id = db.Column(db.Integer, primary_key = True, nullable = False)
+    name = db.Column(db.String(100), nullable = False)
+
+with app.app_context():
+    db.create_all()
 
 users = {"archie.andrews@email.com": "football4life", "veronica.lodge@email.com": "fashiondiva"}
 
@@ -20,8 +43,8 @@ def login():
     if form.validate_on_submit():
         for u_email, u_password in users.items():
             if u_email == form.email.data and u_password == form.password.data:
-                return render_template("login.html",form = form, message ="Successfully Logged In")
-        return render_template("login.html", form = form, message ="Incorrect Email or Password")
+                return render_template("login.html", form=form, message="Successfully Logged In")
+        return render_template("login.html", form=form, message="Incorrect Email or Password")
     elif form.errors:
         print(form.errors.items())
         print(form.email.errors)
