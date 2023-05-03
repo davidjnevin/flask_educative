4e1424fc3100f6b729560021800719739737bcc8
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-05-03 10:16:20 +0200

    Chore: Fix quote marks

--- app.py
+++ app.py
@@ -1,29 +1,34 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from forms import LoginForm
 
 app = Flask(__name__)
+
 app.config["SECRET_KEY"] = "dfewfew123213rwdsgert34tgfd1234trgf"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example_2.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example_2.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+
 class Employee(db.Model):
-    employee_id = db.Column(db.Integer, primary_key = True)
-    first_name = db.Column(db.String(50), nullable = False)
-    last_name = db.Column(db.String(50), nullable = False)
-    department_name = db.Column(db.String(50), db.ForeignKey('department.name'), nullable = False)
-    is_head_of = db.Column(db.String, db.ForeignKey('department.name'), nullable=True)
+    employee_id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    department_name = db.Column(db.String(50), db.ForeignKey("department.name"), nullable=False)
+    is_head_of = db.Column(db.String, db.ForeignKey("department.name"), nullable=True)
+
 
 class Department(db.Model):
-    name = db.Column(db.String(50), primary_key=True, nullable = False)
-    location = db.Column(db.String(120), nullable = False)
-    employees = db.relationship('Employee', backref = 'department')
-    head = db.relationship('Employee', backref='head_of_department', uselist=False)
+    name = db.Column(db.String(50), primary_key=True, nullable=False)
+    location = db.Column(db.String(120), nullable=False)
+    employees = db.relationship("Employee", backref="department")
+    head = db.relationship("Employee", backref="head_of_department", uselist=False)
+
 
 class Project(db.Model):
-    project_id = db.Column(db.Integer, primary_key = True, nullable = False)
-    name = db.Column(db.String(100), nullable = False)
+    project_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+
 
 with app.app_context():
     db.create_all()
